9c4e6d1ae4612cee5b6ed289e4cf869583faf1cf
un0211 <un0211@kaist.ac.kr>
2024-05-01 15:54:25 +0900

    :sparkles:feat: 롤링페이퍼 페이지 구조 작성

---
+++ src/pages/RollingPaperPage.js
@@ -0,0 +1,15 @@
+import CardList from "components/RollingPaperPage/CardList";
+import Nav from "components/RollingPaperPage/Nav";
+
+function RollingPaperPage() {
+  return (
+    <>
+      <Nav />
+      <section>
+        <CardList />
+      </section>
+    </>
+  );
+}
+
+export default RollingPaperPage;
