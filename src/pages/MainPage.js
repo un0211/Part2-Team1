9bf425d6fd151659f9f5ffa1025bff3c13e65603
seo02wow <rlatjdud9188@naver.com>
2024-05-01 16:41:50 +0900

    :lipstick:design: 메인페이지 베너 생성

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,18 +1,20 @@
 import style from "./MainPage.module.scss";
 import rollingExample from "assets/images/rolling_example.svg";
+import reactionExample from "assets/images/reaction_example.png";
 
-function Section() {
+function Section({ imgSrc, point, title, className, imgClassName }) {
   return (
     <section className={style.section}>
-      <div className={style["content-container"]}>
-        <div className={style.emphasis}>Point. 01</div>
-        <h2 className={style.title}>
-          {`누구나 손쉽게, 온라인
-           롤링 페이퍼를 만들 수 있어요`}
-        </h2>
+      <div className={`${style["content-container"]} ${className}`}>
+        <div className={style.emphasis}>{point}</div>
+        <h2 className={style.title}>{title}</h2>
         <p className={style.description}>로그인 없이 자유롭게 만들어요.</p>
       </div>
-      <img src={rollingExample} alt="롤링페이퍼 예시" />
+      <img
+        src={imgSrc}
+        alt="롤링페이퍼 예시"
+        className={`${style[imgClassName]}`}
+      />
     </section>
   );
 }
@@ -20,7 +22,20 @@ function Section() {
 export default function MainPage() {
   return (
     <main className={style.main}>
-      <Section />
+      <Section
+        imgSrc={rollingExample}
+        point={"Point. 01"}
+        title={`누구나 손쉽게, 온라인
+           롤링 페이퍼를 만들 수 있어요`}
+      />
+      <Section
+        imgSrc={reactionExample}
+        point={"Point. 02"}
+        title={`서로에게 이모지로 감정을
+           표현해보세요`}
+        className={"second"}
+        imgClassName={"order"}
+      />
     </main>
   );
 }
