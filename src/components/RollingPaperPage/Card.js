7730e021a6a37121fc1920cd78a78e54d0a90bbb
un0211 <un0211@kaist.ac.kr>
2024-05-01 18:23:33 +0900

    :sparkles:feat: 메세지 추가하는 카드 구현

--- src/components/RollingPaperPage/Card.js
+++ src/components/RollingPaperPage/Card.js
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
+import PlusIcon from "assets/icons/plus.svg";
+import style from "./Card.module.scss";
+
 function Card() {
   return <div></div>;
+}
+
+export function FirstCard() {
+  return (
+    <div className={`${style["rolling-card"]} ${style["rolling-card-first"]}`}>
+      <Link to="message">
+        <div className={style["add-button"]}>
+          <img src={PlusIcon} alt="메세지 추가" />
+        </div>
+      </Link>
+    </div>
+  );
 }
 
 export default Card;
