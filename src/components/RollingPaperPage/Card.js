a9a8daf44a0bfeab0f7ae88838e6865c80d97be5
un0211 <un0211@kaist.ac.kr>
2024-05-02 14:51:59 +0900

    :sparkles:feat: 메세지 카드 틀 생성 (미완성)
    공유를 위해 틀 위주로 작업했습니다.
    - 폰트 미적용
    - 뱃지 구현 X
    - 클릭 이벤트 없음 -> 서영님

--- src/components/RollingPaperPage/Card.js
+++ src/components/RollingPaperPage/Card.js
@@ -1,14 +1,38 @@
 import { Link } from "react-router-dom";
 import PlusIcon from "assets/icons/plus.svg";
 import style from "./Card.module.scss";
+import { formatDateWithDot } from "utils/rollingPaperPage";
 
-function Card() {
-  return <div></div>;
+function Card({ message }) {
+  const { content, createdAt, profileImageURL, relationship, sender } = message;
+
+  return (
+    <article className={style.card}>
+      <header>
+        <img
+          className={style.profile}
+          src={profileImageURL}
+          alt="프로필 이미지"
+        />
+        <div>
+          <h2 className="font_20_regular">
+            From. <span className="font_20_bold">{sender}</span>
+          </h2>
+          <p>{relationship}</p>
+        </div>
+      </header>
+      <div className={style.divider}></div>
+      <main className="font_18_regular">{content}</main>
+      <footer className="font_12_regular">
+        {formatDateWithDot(createdAt)}
+      </footer>
+    </article>
+  );
 }
 
 export function FirstCard() {
   return (
-    <div className={`${style["rolling-card"]} ${style["rolling-card-first"]}`}>
+    <div className={`${style.card} ${style["card-first"]}`}>
       <Link to="message">
         <div className={style["add-button"]}>
           <img src={PlusIcon} alt="메세지 추가" />
