86e7358fe62d18a80bc193380e1de9cb1857b628
seo02wow <rlatjdud9188@naver.com>
2024-05-03 00:42:56 +0900

    :sparkles:feat: 롤링페이퍼 클릭 시 해당 롤링페이퍼 상세 모달창 생성

--- src/components/RollingPaperPage/Card.js
+++ src/components/RollingPaperPage/Card.js
@@ -2,31 +2,41 @@ import { Link } from "react-router-dom";
 import PlusIcon from "assets/icons/plus.svg";
 import style from "./Card.module.scss";
 import { formatDateWithDot } from "utils/rollingPaperPage";
+import CardModal from "./CardModal";
+import { useState } from "react";
 
 function Card({ message }) {
   const { content, createdAt, profileImageURL, relationship, sender } = message;
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const handleCardClick = () => {
+    setIsOpenModal(true);
+  };
 
   return (
-    <article className={style.card}>
-      <header>
-        <img
-          className={style.profile}
-          src={profileImageURL}
-          alt="프로필 이미지"
-        />
-        <div>
-          <h2 className="font_20_regular">
-            From. <span className="font_20_bold">{sender}</span>
-          </h2>
-          <p>{relationship}</p>
-        </div>
-      </header>
-      <div className={style.divider}></div>
-      <main className="font_18_regular">{content}</main>
-      <footer className="font_12_regular">
-        {formatDateWithDot(createdAt)}
-      </footer>
-    </article>
+    <>
+      <article className={style.card} onClick={handleCardClick}>
+        <header>
+          <img
+            className={style.profile}
+            src={profileImageURL}
+            alt="프로필 이미지"
+          />
+          <div>
+            <h2 className="font_20_regular">
+              From. <span className="font_20_bold">{sender}</span>
+            </h2>
+            <p>{relationship}</p>
+          </div>
+        </header>
+        <div className={style.divider}></div>
+        <main className="font_18_regular">{content}</main>
+        <footer className="font_12_regular">
+          {formatDateWithDot(createdAt)}
+        </footer>
+      </article>
+      {isOpenModal && <CardModal message={message} isOpen={isOpenModal} />}
+    </>
   );
 }
 
--- src/components/RollingPaperPage/CardModal.js
+++ src/components/RollingPaperPage/CardModal.js
@@ -1,3 +1,40 @@
-export default function CardModal() {
-  return <></>;
+import Modal from "react-modal";
+import style from "./CardModal.module.scss";
+
+export default function CardModal({ isOpen, message }) {
+  const { content, createdAt, profileImageURL, relationship, sender } = message;
+
+  const modalStyle = {
+    overlay: {
+      backgroundColor: "rgba(0, 0, 0, 0.5)",
+      width: "100%",
+      position: "fixed",
+      top: "0",
+      left: "0",
+      zIndex: "10",
+    },
+    content: {
+      height: "476px",
+      width: "600px",
+      zIndex: "150",
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      borderRadius: "16px",
+    },
+  };
+  return (
+    <Modal isOpen={isOpen} style={modalStyle}>
+      <div className={style["profile-container"]}>
+        <img src={profileImageURL} className={style.profile} />
+        <div>
+          <h3>From. {sender}</h3>
+          <p>{relationship}</p>
+        </div>
+        <p>{createdAt}</p>
+      </div>
+      <p>{content}</p>
+    </Modal>
+  );
 }
