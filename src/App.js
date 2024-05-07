c7ae76289f1d14fbf7555980528ca2f721c507aa
IkJae <dlrwo95@gmail.com>
2024-05-07 18:29:40 +0900

    ✨feat::롤링 페이퍼 페이지 생성 및 기본 스타일 적용

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import MainPage from "pages/MainPage";
 import NotFoundPage from "pages/NotFoundPage";
 import RollingPaperPage from "pages/RollingPaperPage";
 import CreateRollingPage from "pages/CreateRollingPage";
+import Modal from "react-modal";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+Modal.setAppElement("#root");
 
 function App() {
   return (
