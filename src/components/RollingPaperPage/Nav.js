634d5c17818508bc2e043170fa8ac97abeca49f2
un0211 <un0211@kaist.ac.kr>
2024-05-09 00:43:57 +0900

    :sparkles:feat: 롤링페이퍼 페이지 내비게이션 틀 구현

--- src/components/RollingPaperPage/Nav.js
+++ src/components/RollingPaperPage/Nav.js
@@ -1,5 +1,52 @@
-function Nav() {
-  return <nav></nav>;
+import styles from "./Nav.module.scss";
+import arrowDownIcon from "assets/icons/arrow_down.svg";
+import addEmojiIcon from "assets/icons/add_emoji.svg";
+import shareIcon from "assets/icons/share.svg";
+
+function Nav({ postInfo }) {
+  const { name, messageCount, messageProfiles } = postInfo;
+
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.contents}>
+        <div className={`${styles.name} font-28-28-18-bold`}>To. {name}</div>
+        <div className={styles["post-info"]}>
+          <>{messageCount}명이 작성했어요</>
+          <div className={styles.divider}></div>
+          <div className={styles.tools}>
+            <Emojis />
+            <Buttons />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+function Emojis() {
+  return (
+    <div className={styles.emojis}>
+      <>이모지 목록</>
+      <button type="button" className={styles["more-emoji-button"]}>
+        <img src={arrowDownIcon} alt="반응 더보기" />
+      </button>
+    </div>
+  );
+}
+
+function Buttons() {
+  return (
+    <div className={styles.buttons}>
+      <button type="button" className={styles["add-emoji"]}>
+        <img src={addEmojiIcon} alt="반응 추가" />
+        <p>추가</p>
+      </button>
+      <div className={styles.divider}></div>
+      <button type="button">
+        <img src={shareIcon} alt="공유" />
+      </button>
+    </div>
+  );
 }
 
 export default Nav;
