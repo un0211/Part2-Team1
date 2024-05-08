ad4002597340ba67ab52ff2dba3b47b2260b0590
hongseoha <seoseo.ha.h@gmail.com>
2024-05-08 13:11:56 +0900

    :art: style: 따옴표 변경

--- src/apis/ListPage.js
+++ src/apis/ListPage.js
@@ -3,7 +3,7 @@ import { TEAM_BASE_URL } from "constants";
 export async function getList() {
   const response = await fetch(TEAM_BASE_URL);
   if (!response.ok) {
-    throw new Error('데이터를 불러오는데 실패했습니다');
+    throw new Error("데이터를 불러오는데 실패했습니다");
   }
   const body = await response.json();
   return body;
--- src/components/RollingPaperPage/Card.js
+++ src/components/RollingPaperPage/Card.js
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
 import PlusIcon from "assets/icons/plus.svg";
-import style from "./Card.module.scss";
+import styles from "./Card.module.scss";
 import { formatDateWithDot } from "utils/rollingPaperPage";
 
 function Card({ message }) {
   const { content, createdAt, profileImageURL, relationship, sender } = message;
 
   return (
-    <article className={style.card}>
+    <article className={styles.card}>
       <header>
         <img
-          className={style.profile}
+          className={styles.profile}
           src={profileImageURL}
           alt="프로필 이미지"
         />
@@ -21,7 +21,7 @@ function Card({ message }) {
           <p>{relationship}</p>
         </div>
       </header>
-      <div className={style.divider}></div>
+      <div className={styles.divider}></div>
       <main className="font_18_regular">{content}</main>
       <footer className="font_12_regular">
         {formatDateWithDot(createdAt)}
@@ -32,9 +32,9 @@ function Card({ message }) {
 
 export function FirstCard() {
   return (
-    <div className={`${style.card} ${style["card-first"]}`}>
+    <div className={`${styles.card} ${styles["card-first"]}`}>
       <Link to="message">
-        <div className={style["add-button"]}>
+        <div className={styles["add-button"]}>
           <img src={PlusIcon} alt="메세지 추가" />
         </div>
       </Link>
