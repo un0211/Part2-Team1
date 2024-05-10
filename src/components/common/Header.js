c7b15da444a55a8958d718cc0b75e0b7b8f73c85
seo02wow <rlatjdud9188@naver.com>
2024-05-10 21:18:29 +0900

    :sparkles:feat: 모바일에서 메인페이지랑 리스트 페이지가 아닌 경우 Header 숨김 처리

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,12 +1,39 @@
 import logo from "assets/icons/logo.svg";
 import styles from "./Header.module.scss";
 import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Header() {
   const location = useLocation();
+  const [isHeaderVisible, setIsHeaderVisible] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (
+        window.innerWidth <= 768 &&
+        !["/", "/list"].includes(location.pathname)
+      ) {
+        setIsHeaderVisible(false);
+      } else {
+        setIsHeaderVisible(true);
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [location.pathname]);
 
   return (
-    <header className={styles.header}>
+    <header
+      className={`${styles.header} ${
+        isHeaderVisible ? styles.visible : styles.hidden
+      }`}
+    >
       <div className={styles["logo-button-container"]}>
         <Link to="/" className={styles["logo-container"]}>
           <img src={logo} className={styles.logo} alt="로고" />
