112fda24088314674d0c34895f572690fd0ebd86
hongseoha <seoseo.ha.h@gmail.com>
2024-05-12 02:45:44 +0900

    :sparkles: feat: background에 따라 스타일 변경

--- src/components/ListPage/CardList.js
+++ src/components/ListPage/CardList.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import styles from "./CardList.module.scss";
-import CountMessage from "components/common/CountMessage";
-import TopReaction from "components/common/TopReaction";
-import { LIST_PAGE } from "constants";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './CardList.module.scss';
+import CountMessage from 'components/common/CountMessage';
+import TopReaction from 'components/common/TopReaction';
+import { LIST_PAGE } from 'constants';
 
 function CardList({ slideItems }) {
   const {
@@ -25,19 +25,24 @@ function CardList({ slideItems }) {
     backgroundImage: `url(${backgroundImageURL})`,
   };
 
+  const backgroundClass = backgroundImageURL ? 'bg-url' : 
+                      backgroundColor === 'purple' ? 'bg-purple' :
+                      backgroundColor === 'beige' ? 'bg-yellow' :
+                      backgroundColor === 'green' ? 'bg-green' :
+                      'bg-blue';
   return (
-    <div className={styles.CardList} style={cardStyle}>
-      <Link to={`/post/${id}`} className={styles.CardList}>
-        <h3 className={`font-24-bold`}>To. {name}</h3>
-        <CountMessage
-          messageProfiles={messageProfiles}
-          messageCount={messageCount}
-          page={LIST_PAGE}
-        />
-        <TopReaction
-        topReactions={topReactions}
-        />
-      </Link>
+    <div className={styles["CardList"]} style={cardStyle}>
+      <div className={styles[backgroundClass]}>
+        <Link to={`/post/${id}`} className={styles.CardList}>
+          <h3 className={`font-24-bold`}>To. {name}</h3>
+          <CountMessage
+            messageProfiles={messageProfiles}
+            messageCount={messageCount}
+            page={LIST_PAGE}
+          />
+          <TopReaction topReactions={topReactions} />
+        </Link>
+      </div>
     </div>
   );
 }
