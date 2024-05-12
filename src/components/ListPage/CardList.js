ec2d8c4881b2f8428ab6b08e465b6e158c15b365
hongseoha <seoseo.ha.h@gmail.com>
2024-05-13 03:40:30 +0900

    :art: refactor: 삼항연산자 단순화

--- src/components/ListPage/CardList.js
+++ src/components/ListPage/CardList.js
@@ -26,11 +26,8 @@ function CardList({ slideItems }) {
     backgroundImage: `url(${backgroundImageURL})`,
   };
 
-  const backgroundClass = backgroundImageURL ? "bg-url" : 
-                      backgroundColor === "purple" ? "bg-purple" :
-                      backgroundColor === "beige" ? "bg-yellow" :
-                      backgroundColor === "green" ? "bg-green" :
-                      "bg-blue";
+  const backgroundClass = backgroundImageURL ? "bg-url" : `bg-${backgroundColor}`
+
   return (
     <div className={styles["CardList"]} style={cardStyle}>
       <div className={`${styles["bg-setting"]} ${styles[backgroundClass]}`}>
