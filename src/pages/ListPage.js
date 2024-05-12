213acc6069d547230905ffee2698e661914cb134
seo02wow <rlatjdud9188@naver.com>
2024-05-12 15:53:08 +0900

    :sparkles:feat: /list 페이지 로딩 처리

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,38 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import { getList } from 'apis/ListPage';
-import Carousel from '../components/ListPage/Carousel';
-import './ListPage.module.scss';
-import styles from './ListPage.module.scss';
-import 'styles/global.css';
-import 'styles/button.scss';
-import LinkButton from 'components/common/LinkButton';
+import React, { useEffect, useState } from "react";
+import { getList } from "apis/ListPage";
+import Carousel from "../components/ListPage/Carousel";
+import "./ListPage.module.scss";
+import styles from "./ListPage.module.scss";
+import "styles/global.css";
+import "styles/button.scss";
+import LinkButton from "components/common/LinkButton";
 
 function ListPage() {
   const [bestItems, setBestItems] = useState([]);
   const [recentItems, setRecentItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setIsLoading(true);
         const response = await getList();
         const items = response.results;
-        const sortedBest = items.slice().sort((a, b) => b.messageCount - a.messageCount);
-        const sortedRecent = items.slice().sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+        const sortedBest = items
+          .slice()
+          .sort((a, b) => b.messageCount - a.messageCount);
+        const sortedRecent = items
+          .slice()
+          .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
         setBestItems(sortedBest);
         setRecentItems(sortedRecent);
       } catch (error) {
-        console.error('Error fetching slide items:', error);
+        console.error("Error fetching slide items:", error);
       }
     };
-
+    setIsLoading(false);
     fetchData();
   }, []);
 
   return (
     <div className={styles.wrapper}>
       <div className={styles.track}>
-        <Carousel title="인기 롤링 페이퍼🔥" slideItems={bestItems} />
-        <Carousel title="최근에 만든 롤링 페이퍼⭐" slideItems={recentItems} />
+        <Carousel
+          title="인기 롤링 페이퍼🔥"
+          slideItems={bestItems}
+          isLoading={isLoading}
+        />
+        <Carousel
+          title="최근에 만든 롤링 페이퍼⭐"
+          slideItems={recentItems}
+          isLoading={isLoading}
+        />
         <div className={styles[`button-wrapper`]}>
           <LinkButton
             className={styles.custom}
