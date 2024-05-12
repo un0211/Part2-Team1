393492f595efacc3662bd2fbca1f8feee0f5c052
seo02wow <rlatjdud9188@naver.com>
2024-05-13 03:13:28 +0900

    :sparkles:feat: input value state에 저장

--- src/pages/CreateRollingPaperPage.js
+++ src/pages/CreateRollingPaperPage.js
@@ -8,14 +8,54 @@ import {
 
 export default function CreateRollingPaPer() {
   const [selectedBg, setSelectedBg] = useState("color");
+  const [backgroundColor, setBackgroundColor] = useState("beige");
+  const [backgroundImg, setBackgroundImg] = useState(null);
+  const [name, setName] = useState("");
 
   // NOTE - 배경 컬러, 이미지 중 선택하는 함수
   const handleBgSelect = (type) => {
     setSelectedBg(type);
+
+    // NOTE - 배경 컬러인 경우 첫 번째 배경 색상으로 설정
+    if (type === "color") {
+      setBackgroundColor(BACKGROUND_COLOR[0]);
+    }
+    // NOTE - 배경 이미지인 경우 첫 번째 배경 이미지로 설정
+    else if (type === "image") {
+      setBackgroundImg(BACKGROUND_IMAGE.first);
+    }
+  };
+
+  // NOTE - 배경 선택하는 함수
+  const handleBackgroundSelect = (e) => {
+    if (selectedBg === "color") {
+      setBackgroundColor(e.target.value);
+    } else {
+      setBackgroundImg(e.target.value);
+    }
+  };
+
+  // NOTE - name Input
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+      team: "6-1",
+      name: name,
+      backgroundColor: backgroundColor,
+      backgroundImageURL: backgroundImg,
+    };
+
+    console.log("넘어가는 데이터 확인 : " + data.name);
+    console.log("넘어가는 데이터 확인 : " + data.backgroundColor);
+    console.log("넘어가는 데이터 확인 : " + data.backgroundImageURL);
   };
 
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <section className={styles["sender-container"]}>
         <label htmlFor="sender" className="font-24-24-24-bold">
           To.
@@ -25,6 +65,7 @@ export default function CreateRollingPaPer() {
           name="sender"
           placeholder="받는 사람 이름을 입력해 주세요."
           className={styles["sender-input"]}
+          onChange={handleName}
         />
       </section>
       <section className={styles["select-bg-section"]}>
@@ -37,9 +78,17 @@ export default function CreateRollingPaPer() {
         <BackgroundButton onBgSelect={handleBgSelect} selectedBg={selectedBg} />
         <div className={styles["select-bg-input-container"]}>
           {selectedBg === "color" ? (
-            <Background backgrounds={BACKGROUND_COLOR} name="color" />
+            <Background
+              backgrounds={BACKGROUND_COLOR}
+              name="color"
+              onBackgroundSelect={handleBackgroundSelect}
+            />
           ) : (
-            <Background backgrounds={BACKGROUND_IMAGE_NAME} name="image" />
+            <Background
+              backgrounds={BACKGROUND_IMAGE_NAME}
+              name="image"
+              onBackgroundSelect={handleBackgroundSelect}
+            />
           )}
         </div>
       </section>
@@ -77,17 +126,19 @@ function BackgroundButton({ onBgSelect, selectedBg }) {
   );
 }
 
-function Background({ backgrounds, name }) {
+function Background({ backgrounds, name, onBackgroundSelect }) {
   return (
     <>
-      {backgrounds.map((background) => (
+      {backgrounds.map((background, index) => (
         <>
           <input
             type="radio"
             name={name}
             id={background}
-            value={name === "color" ? background : BACKGROUND_IMAGE.background}
+            value={name === "color" ? background : BACKGROUND_IMAGE[background]}
             className={styles["select-bg-input"]}
+            onChange={onBackgroundSelect}
+            key={index}
           />
           <label
             htmlFor={background}
