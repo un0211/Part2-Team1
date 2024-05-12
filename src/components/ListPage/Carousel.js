9c11e60ed22ec4402bef45822d197d3b95554167
SeohaH <122506809+hongseoha@users.noreply.github.com>
2024-05-12 14:56:16 +0900

    [ListPage] 조건에 따라 좌우 버튼 활성화 변경 및 코드 리팩토링 (#36)
    * :lipstick: style: 제목 및 캐러셀 위치 변경
    
    * :lipstick: style: 제목, 캐러셀 위치 재변경
    
    * :lipstick: style: 제목/캐러셀 위치 조정 최종
    
    * :sparkles: feat: 조건에 따라 화살표 버튼 visivility 변화
    
    * :art: refactor: 중복되는 코드 제거
    
    * :art: refactor: 중복 코드 제거(캐러셀 하나로 통합)
    
    * :art: refactor: length prop으로 전달

---
+++ src/components/ListPage/Carousel.js
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import Slider from 'react-slick';
+import styles from './Carousel.module.scss';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import image_prev from 'assets/icons/arrow_prev.png';
+import image_next from 'assets/icons/arrow_next.png';
+import CardList from './CardList';
+
+function Carousel({ title, slideItems }) {
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const length = slideItems.length;
+
+  const settings = {
+    dots: false,
+    infinite: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    draggable: true,
+    nextArrow: <NextArrow currentSlide={currentSlide} slideItems={slideItems} length={length}/>,
+    prevArrow: <PrevArrow currentSlide={currentSlide} />,
+    afterChange: (index) => setCurrentSlide(index),
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1>{title}</h1>
+      <Slider {...settings} className={styles.slider}>
+        {slideItems?.map((item) => (
+          <CardList key={item.id} slideItems={item} />
+        ))}
+      </Slider>
+    </div>
+  );
+}
+
+
+function NextArrow({ custom, styles, onClick, currentSlide, length }) {
+  return (
+    <div
+      className={custom}
+      style={{
+        ...styles,
+        display: 'block',
+        width: '40px',
+        height: '40px',
+        right: '-1130px',
+        top: '-150px',
+        borderRadius: '50%',
+        position: 'relative',
+        visibility:
+          length > 4 && currentSlide + 4 < length
+            ? 'visible'
+            : 'hidden',
+      }}
+      onClick={onClick}
+    >
+      <img
+        src={image_next}
+        alt="Next Arrow"
+        style={{
+          width: '60px',
+          height: '60px',
+          borderRadius: '50%',
+          objectFit: 'cover',
+        }}
+      />
+    </div>
+  );
+}
+function PrevArrow(props) {
+  const { custom, styles, onClick, currentSlide } = props;
+
+  return (
+    <div
+      className={custom}
+      style={{
+        ...styles,
+        display: 'block',
+        width: '40px',
+        height: '40px',
+        left: '-30px',
+        top: '150px',
+        borderRadius: '50%',
+        position: 'relative',
+        zIndex: '3',
+        visibility: currentSlide === 0 ? 'hidden' : 'visible',
+      }}
+      onClick={() => {
+        if (currentSlide !== 0) onClick();
+      }}
+    >
+      <img
+        src={image_prev}
+        alt="Previous Arrow"
+        style={{
+          width: '60px',
+          height: '60px',
+          borderRadius: '50%',
+          objectFit: 'cover',
+        }}
+      />
+    </div>
+  );
+}
+
+export default Carousel;
