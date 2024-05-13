958c7f4f2296972dc68b6ecf9826bd95167750a9
seo02wow <rlatjdud9188@naver.com>
2024-05-13 20:17:05 +0900

    :recycle:refactor: 배경 선택, 배경 이미지 컴포넌트 폴더 분리

---
+++ src/components/CreateRollingPaperPage/BackgroundButton.js
@@ -0,0 +1,26 @@
+export default function BackgroundButton({ onBgSelect, selectedBg }) {
+  return (
+    <div className={styles["select-bg-container"]}>
+      <input
+        type="radio"
+        id="bgColor"
+        name="select"
+        checked={selectedBg === "color"}
+        onChange={() => onBgSelect("color")}
+      />
+      <label htmlFor="bgColor" className={`font-16-16-16`}>
+        컬러
+      </label>
+      <input
+        type="radio"
+        id="bgImg"
+        name="select"
+        checked={selectedBg === "image"}
+        onChange={() => onBgSelect("image")}
+      />
+      <label htmlFor="bgImg" className={`font-16-16-16`}>
+        이미지
+      </label>
+    </div>
+  );
+}
