4a4e554e10db9279c7630170af3bd8b1cee15e96
김세민 <ksm6430@naver.com>
2024-05-14 07:15:42 +0900

    :recycle:refactor Input 컴포넌트에 값을 입력하지 않으면 error 메세지, PostMessagePage.module.scss위치 이동

--- src/pages/PostMessagePage.js
+++ src/pages/PostMessagePage.js
@@ -1,35 +1,50 @@
-// PostMessageForm.js
 import React, { useState } from "react";
 import { EditorState } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
 import { stateToHTML } from "draft-js-export-html";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import styles from "styles/PostMessagePage.module.scss";
+import styles from "pages/PostMessagePage.module.scss";
 import CustomDropdown from "components/CreateMessage/CustomDropdown";
 import ProfileSelect from "components/CreateMessage/ProfileSelect";
-import { FONT_CLASS_NAME, MEMBER_CLASS_NAME, PROFILES } from "constants/postMessagePage";
+import {
+  FONT_CLASS_NAME,
+  MEMBER_CLASS_NAME,
+  PROFILES,
+} from "constants/postMessagePage";
 import { useParams, useNavigate } from "react-router-dom";
 import { putMessage } from "apis/postMessagePage";
 
 export default function PostMessageForm() {
   const [senderValue, setSenderValue] = useState("");
+  const [senderError, setSenderError] = useState(false);
   const [relationship, setRelationship] = useState("지인");
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const [editorError, setEditorError] = useState(false);
   const [selectedFont, setSelectedFont] = useState("Noto Sans");
   const [selectedProfile, setSelectedProfile] = useState(PROFILES[0]);
+  const [editorContent, setEditorContent] = useState("");
+  const [senderContent, setSenderContent] = useState("");
 
   // NOTE - id 받아오는 작업
   const { postId } = useParams();
   const navigate = useNavigate();
 
   const handleNameChange = (e) => {
-    setSenderValue(e.target.value);
+    const name = e.target.value.trim(); //NOTE 공백을 제거하여 입력값 확인
+    setSenderValue(name);
+    setSenderContent(name); //NOTE 보낸이 내용을 상태에 업데이트
+    setSenderError(name === ""); //NOTE 공백이면 에러 상태를 true로 설정
   };
 
   const handleEditorChange = (state) => {
     setEditorState(state);
-    console.log(state.getCurrentContent().getPlainText("\n"));
+    const content = state.getCurrentContent().getPlainText("\n");
+    setEditorContent(content); //NOTE 에디터 내용을 상태에 업데이트
+    setEditorError(content.trim() === "");
   };
+
+  const isButtonDisabled =
+    !editorContent.trim() || !senderContent || senderError || editorError;
 
   const handleProfileSelect = (profile) => {
     setSelectedProfile(profile);
@@ -41,7 +56,6 @@ export default function PostMessageForm() {
       .replace(/<\/p>/g, "")
       .replace(/<strong>/g, "")
       .replace(/<\/strong>/g, "");
-
   };
 
   const handleSubmit = async (e) => {
@@ -66,20 +80,27 @@ export default function PostMessageForm() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className={styles["message-form"]}>
+      <div
+        className={`${styles["message-form"]} ${senderError ? "error" : ""}`}
+      >
         <div className={styles["message-form-sender"]}>
           <label
             htmlFor="nameInput"
-            className={`${styles["message-form-title"]} ${styles["font-28-28-18-bold"]}`}
+            className={`${styles["message-form-title"]}`}
           >
             From.
           </label>
           <input
             onChange={handleNameChange}
-            className={`${styles["message-form-inputs"]} ${styles["message-form-name-input"]} ${styles["font-28-28-18-bold"]}`}
+            className={`${styles["message-form-inputs"]} ${
+              styles["message-form-name-input"]
+            } ${senderError ? styles.error : ""}`}
             id="nameInput"
             placeholder="이름을 입력해 주세요."
           />
+          {senderError && (
+            <p className={styles["form-error"]}>값을 입력해주세요.</p>
+          )}{" "}
         </div>
 
         <ProfileSelect onProfileSelect={handleProfileSelect} />
@@ -93,19 +114,29 @@ export default function PostMessageForm() {
             onSelect={(value) => setRelationship(value)}
           />
         </div>
-        <div className={styles["message-form-content"]}>
+        <div
+          className={`${styles["message-form-content"]} ${
+            editorError ? styles.error : ""
+          }`}
+        >
           <label htmlFor="textarea" className={styles["message-form-title"]}>
             내용을 입력해 주세요
           </label>
+
           <Editor
             editorState={editorState}
             onEditorStateChange={handleEditorChange}
             wrapperClassName={styles["message-form-text-editor-wrapper"]}
-            editorClassName={styles["message-form-text-editor"]}
+            editorClassName={`${styles["message-form-text-editor"]} ${
+              editorError ? styles.error : ""
+            }`}
             toolbar={{
               options: ["inline", "textAlign", "emoji", "remove", "history"],
             }}
           />
+          {editorError && (
+            <p className={styles["form-error"]}>값을 입력해주세요.</p>
+          )}
         </div>
 
         <div className={styles["message-form-font"]}>
@@ -115,10 +146,15 @@ export default function PostMessageForm() {
             onSelect={(value) => setSelectedFont(value)}
           />
         </div>
+
+        <button
+          className={`${styles["message-form-submit"]}`}
+          type="submit"
+          disabled={isButtonDisabled}
+        >
+          생성하기
+        </button>
       </div>
-      <button className={styles["message-form-submit"]} type="submit">
-        생성하기
-      </button>
     </form>
   );
 }
