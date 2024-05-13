07726fd156745ea217a93f359662e4afa39291eb
ssseeo0 <135966211+ssseeo0@users.noreply.github.com>
2024-05-13 22:10:54 +0900

    [Create Page] 리팩토링 작업  (#51)
    * :recycle:refactor: 배경 선택, 배경 이미지 컴포넌트 폴더 분리
    
    * :recycle:refactor: 컴포넌트 및 css 파일 분리
    
    * :bulb:comment: 불필요한 주석, console 제거
    
    * :recycle:refactor: 상수 사용 및 주석 수정
    
    * :art:rename: post 페이지 상수 파일명 변경
    
    * :rewind:fix: 실수로 지운 코드 복구

---
+++ src/components/CreateRollingPaperPage/Background.js
@@ -0,0 +1,52 @@
+import { BACKGROUND_IMAGE } from "constants/createRollingPaperPage";
+import selected from "assets/icons/selected.svg";
+import styles from "./Background.module.scss";
+
+export default function Background({
+  backgrounds,
+  name,
+  onBackgroundSelect,
+  checkedValue,
+}) {
+  return (
+    <>
+      {backgrounds.map((background, index) => (
+        <>
+          <input
+            type="radio"
+            name={name}
+            id={background}
+            value={name === "color" ? background : BACKGROUND_IMAGE[background]}
+            className={styles["select-bg-input"]}
+            onChange={onBackgroundSelect}
+            key={index}
+          />
+          <label
+            htmlFor={background}
+            className={`${styles[background]} ${styles["select-bg-label"]}`}
+          >
+            <div
+              className={`${styles["checked-container"]} ${
+                checkedValue ===
+                (name === "color" ? background : BACKGROUND_IMAGE[background])
+                  ? styles["checked-background-opacity"]
+                  : ""
+              }`}
+            >
+              <img
+                src={selected}
+                alt="선택 아이콘"
+                className={`${
+                  checkedValue ===
+                  (name === "color" ? background : BACKGROUND_IMAGE[background])
+                    ? ""
+                    : styles["none-checked-background"]
+                }`}
+              />
+            </div>
+          </label>
+        </>
+      ))}
+    </>
+  );
+}
