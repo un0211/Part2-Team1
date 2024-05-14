18c9986117ec88188ba8d0e2f5bb3fe95dc5a1c5
un0211 <un0211@kaist.ac.kr>
2024-05-14 17:35:56 +0900

    :sparkles:feat: 전체보기 페이지 기본 구현

--- src/pages/AllPostPage.js
+++ src/pages/AllPostPage.js
@@ -1,40 +1,59 @@
-import React, { useEffect, useState } from "react";
-import { getPost } from "apis/recipients";
-import styles from "./AllPostPage.module.scss";
-import "styles/global.css";
-import "styles/button.scss";
+import { useEffect, useState, useCallback } from "react";
+import { getList } from "apis/recipients";
 import CardList from "components/ListPage/CardList";
-import { Link } from "react-router-dom";
+import styles from "./AllPostPage.module.scss";
+import Loading from "components/common/Loading";
 
-function AllPostPage(){
-  const [allItems, setAllItems] = useState([]);
+function AllPostPage() {
+  const [itemInfo, setItemInfo] = useState({
+    items: [],
+    ids: [],
+    count: 0,
+    offset: 0,
+  });
   const [isLoading, setIsLoading] = useState(false);
+  const [loadingError, setLoadingError] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setIsLoading(true);
-        const response = await getPost();
-        const items = response.results;
-        setAllItems = items;
+  const handleInitLoad = useCallback(async () => {
+    let response;
+    try {
+      setLoadingError(null);
+      setIsLoading(true);
+      response = await getList();
+    } catch (e) {
+      setLoadingError(e);
+      return;
+    }
 
-      } catch (error) {
-        console.error("Error fetching slide items:", error);
-      }
-      setIsLoading(false);
-    };
-    fetchData();
+    setIsLoading(false);
+    const { results: newItems, count } = response;
+    setItemInfo({
+      items: newItems,
+      ids: newItems.map((item) => item.id),
+      count,
+      offset: newItems.length, // NOTE - 다음에 여기부터 받으면 된다
+    });
   }, []);
 
-  return(
-    <div className={styles.wrapper}>
-      {allItems?.map((item) => (
-        <CardList key={item.id} slideItems={item}/>
-      ))}
+  useEffect(() => {
+    handleInitLoad();
+  }, [handleInitLoad]);
 
-    </div>
-
-  )
+  return (
+    <main>
+      <section className={styles["card-section"]}>
+        <ol className={styles["card-list"]}>
+          {itemInfo.items.map((item) => (
+            <li key={item.id}>
+              <CardList slideItems={item} />
+            </li>
+          ))}
+        </ol>
+        {isLoading && <Loading />}
+        {loadingError?.message ? <p>{loadingError.message}</p> : ""}
+      </section>
+    </main>
+  );
 }
 
 export default AllPostPage;
