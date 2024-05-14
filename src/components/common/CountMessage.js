652a9569385bb1de5907af2c5fce608b0fb6550d
un0211 <un0211@kaist.ac.kr>
2024-05-14 15:52:07 +0900

    [postPage] 디자인과 간단한 수정 (#49)
    * :heavy_plus_sign:add: 삭제된 plus 아이콘 추가
    
    * :art:fix: 플러스 카드 전체를 a태그로 변경
    
    * :lipstick:design: 반응이 없을 때 디자인 개선
    
    * :lipstick:design: 메세지 없을 때 문구 변경
    
    * :lipstick:design: 이모지 피커 모바일 위치 고정
    
    * :lipstick:design: 반응 목록 디자인 개선
    
    - 반응 없을 때 글자 크기 설정
    - 버튼 이미지 가운데정렬

--- src/components/common/CountMessage.js
+++ src/components/common/CountMessage.js
@@ -3,32 +3,29 @@ import styles from "./CountMessage.module.scss";
 function CountMessage({ messageProfiles, messageCount, page }) {
   return (
     <div className={styles[page]}>
-      {messageCount === 0 ? (
-        <div className={`${styles["no-message"]} font-16-16-14`}>
-          첫 메시지를 남겨주세요!
-        </div>
-        
-      ) : (
-        <>
-          <ol className={styles.profiles}>
-            {messageProfiles &&
-              messageProfiles.map((profile) => (
-                <li key={profile.id} className={styles.profile}>
-                  <img src={profile.imgURL} alt={`프로필 이미지`} />
-                </li>
-              ))}
-            {messageCount > 3 && (
-              <div className={`${styles["extra-profile"]} font-12-12-12`}>
-                {" "}
-                +{messageCount - 3}
-              </div>
-            )}
-          </ol>
-          <div className={`${styles["message-count"]} font-16-16-14`}>
-            <span className={styles.bold}>{messageCount}</span>명이 작성했어요!
+      <ol className={styles.profiles}>
+        {messageProfiles &&
+          messageProfiles.map((profile) => (
+            <li key={profile.id} className={styles.profile}>
+              <img src={profile.imgURL} alt={`프로필 이미지`} />
+            </li>
+          ))}
+        {messageCount > 3 && (
+          <div className={`${styles["extra-profile"]} font-12-12-12`}>
+            {" "}
+            +{messageCount - 3}
           </div>
-        </>
-      )}
+        )}
+      </ol>
+      <div className={`${styles["message-count"]} font-16-16-14`}>
+        {messageCount ? (
+          <>
+            <span className={styles.bold}>{messageCount}</span>명이 작성했어요!
+          </>
+        ) : (
+          <>첫 메세지를 남겨보세요!</>
+        )}
+      </div>
     </div>
   );
 }
